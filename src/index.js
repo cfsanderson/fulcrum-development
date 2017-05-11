923c38efe783a2e6863dfc7a07e4f331d84ce1b7
Caleb Sanderson <calebsanderson@gmail.com>
2017-05-11 15:19:02 -0400

    Basic Fulcrum styles in screen.scss are now importing. Basic styling for the Security page is working. Need to work on importing the header into pages.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const main = () => {
 
 document.addEventListener('DOMContentLoaded', main)
 
+function supportsImports () {
+  return 'import' in document.createElement('link')
+}
+
+if (supportsImports()) {
+  console.log('Good to go!')
+} else {
+  console.log('Use other libraries/require systems to load files.')
+}
+
 // HERE BE DRAGONS... and webpack. Don't touch.
 if (process.env.NODE_ENV !== 'production') require('./index.html')
 if (module.hot) {
